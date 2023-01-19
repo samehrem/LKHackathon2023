0ae65af2ebcbef619b85e5e5bd8f60c5eb6fbd43
Mehrem <s.l.mehrem@uu.nl>
2023-01-19 18:47:01 +0100

    Updated draft

--- HackathonScriptDraft.R
+++ HackathonScriptDraft.R
@@ -13,13 +13,11 @@ library(lm)
 ############
 
 ####Loading in your raw phenotype data
-raw.phenotypes.day1.rep1 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phe_sat_rep1_1106.xlsx")
-raw.phenotypes.day2.rep1 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phe_sat_rep1_2506.xlsx")
-raw.phenotypes.day1.rep2 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phe_sat_rep2_1106.xlsx")
-raw.phenotypes.day2.rep2 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phe_sat_rep2_2506.xlsx")
-##Combining reps
-raw.phenotypes.day1 <- rbind(raw.phenotypes.day1.rep1,raw.phenotypes.day1.rep2)
-raw.phenotypes.day2 <- rbind(raw.phenotypes.day2.rep1,raw.phenotypes.day2.rep2)
+raw.phenotypes.rep1 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Hackathon_Phenotypes_Rep1.xlsx")
+raw.phenotypes.rep2 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Hackathon_Phenotypes_Rep2.xlsx")
+
+##Combining replicates
+raw.phenotypes <- rbind(raw.phenotypes.rep1,raw.phenotypes.rep2)
 
 #raw.phenotypes.X <- read.xlsx("PathToExcelSheet") ###Example script for if people want to upload their own phenotypes.
 
@@ -27,8 +25,8 @@ raw.phenotypes.day2 <- rbind(raw.phenotypes.day2.rep1,raw.phenotypes.day2.rep2)
 ####Exploring the phenotypes
 
 #Q1: What phenotypes did we load? How many samples do we have?
-colnames(raw.phenotypes.day1.rep1) #Different phenotype names
-nrow(raw.phenotypes.day1.rep1) #Number of LK accessions
+colnames(raw.phenotypes.rep1) #Different phenotype names
+nrow(raw.phenotypes.rep1) #Number of LK accessions
 
 #Q2: What are the distributions of our phenotypes (by horticultural type)?
 lk.ID <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Species_info.xlsx") ###We load in information about the LK accessions
@@ -37,11 +35,7 @@ table(lk.subtype) #We look at what subtypes are represented in our set
 ##CODE PLOT ## Here Basten if you have code for that I am happy if you want to insert it here
 
 #Q3: What are the mean and median values per LK accession/Horticultural type/replicate?
-mean.d1 <- apply(raw.phenotypes.day1[,-c(1,412)],2,mean)
-mean.d2 <- apply(raw.phenotypes.day2.rep1[,-c(1,480)],2,mean)
-
-med.d1 <- apply(raw.phenotypes.day1[,-c(1,412)],2,median)
-med.d2 <- apply(raw.phenotypes.day2[,-c(1,480)],2,median)
+##TODO
 
 
 #####Calculating broad sense heritability (BSH)
@@ -51,19 +45,28 @@ med.d2 <- apply(raw.phenotypes.day2[,-c(1,480)],2,median)
 
 ###@Basten, I hope this is the correct way....
 
-day1.res <- lm(raw.phenotypes.day1$height.mean~raw.phenotypes.day1$accession)
-day1.res.anova <- anova(day1.res)
-BSH <- day1.res.anova$`Mean Sq`[1]/(day1.res.anova$`Mean Sq`[1]+day1.res.anova$`Mean Sq`[2]) ##Make this a function and then apply?
+pheno.res <- lm(raw.phenotypes$relblue.mean~raw.phenotypes$accession)
+pheno.res.anova <- anova(pheno.res)
+BSH <- pheno.res.anova$`Mean Sq`[1]/(pheno.res.anova$`Mean Sq`[1]+pheno.res.anova$`Mean Sq`[2]) ##Make this a function and then apply?
 BSH
 
 
 ## Our phenotypes are not ready for GWAS yet. We need to normalise the trait (if needed) and prepare it
-## to be used as an input for our GWAS script.
+## to be used as an input for our GWAS script. For now we will create a phenotype matrix where rows are the phenotypes
+## and columns are the LK accessions
 
-#Q5: What type of measurement do we have? It can be counts, ratios, binary/ordinal values or quantitative measurements.
+
+#Q5: What type of measurement do we have? It can be counts, ratios, binary/ordinal values or 
+## quantitative measurements.We have replicates so we also have to first take the average across the replicates
 ###INSERT CODE and EXPLANATION FOR NORMALISATION
 
+##Here i have some questions: for height, red/blue values etc we do log2? 
+phenotypes.diff.mean <- (raw.phenotypes.rep1[,2:5]+raw.phenotypes.rep2[,2:5])/2
+rownames(phenotypes.diff.mean) <- raw.phenotypes.rep1$accession
+phenotypes.diff.mean <- t(phenotypes.diff.mean)
 #Q6: Save phenotype file as input file for GWAS script.
+
+save(phenotypes.diff.mean,file="/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phenotypes_for_GWAS.out")
 
 ###Insert CODE for saving
 
@@ -98,25 +101,145 @@ library(dplyr)
 ## During GWAS we use linear models to test the association of a genetic variant (SNP,PAV,CNV,kmer) 
 ## with the trait of interest. For this workshop we run GWAS with SNPs.
 
-#Q1: How are SNPs, CNVs and PAVs distributed across the genome?
+#Q1: Load the SNP file into R
 
-##INSERT CODE FOR COMPARISON (histogram across genome) (If we can load all types of variants at once, otherwise we just use SNPs)
+load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/obj_bgi.sat.snps.hck.out")
+snp.info <- sat.snps[,1:3]
+sat.snps <- sat.snps[,4:134]
 
 ##One important step is calculating a kinship matrix. The kinship matrix is used during the GWAS to correct
-##for false positive associations by taking into account population structure.
+##for false positive associations by taking into account population structure. We can get an estimate of kinship
+##calculating the covariance of the SNPs.
 
-#Q2: Calculating kinship using SNPs. Visualising the population structure, including the horticultural types
+##NOT POSSIBLE ON A LAPTOP so they will load the kinship
 
-##INSERT CODE
+load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/BGI_Sat_kinship.out")
+heatmap(letkin)
 
 ## We prepared our phenotype input and saved it as [FILENAME]
 
-#Q2: Import phenotype file 
-##load(BLA)
-
 ## Below you will find the GWAS script we will use.
 
-###INSERT GWAS code
+GWAS <- function(genotypes, trait, phenotype.name, kinship, out.dir,
+                 give.pval.output.in.R = F, maf.thr = 0.95) {
+  
+  ## Prep trait/phenotypes
+  phenotype <- toString(phenotype.name)
+  letkin <- kinship
+  usemat <- genotypes
+  selc <- !is.na(trait) #Selects the lines with an observation (removes lines that have an NA)
+  trait.names <- trait[selc]
+  use.trait <- trait[selc]
+  print("Traits are selected.")
+  print(paste("The phenotype ID is ", phenotype,".", sep=""))
+  print(max(usemat,na.rm = T))
+  
+  ## Filter in usemat and kinship object for mapping
+  usemat <- usemat[,selc]
+  letkin <- letkin[selc,selc]
+  print(dim(letkin))
+  ## Filter again MAF 5%
+  threshold <- round(ncol(usemat)*maf.thr, digits=0)
+  print(threshold)
+  maf.filter.quick <- apply(usemat == 1,1,sum)> threshold  | apply(usemat == 0,1,sum) > threshold ### <-- this is quicker!
+  print(paste0("SNPs falling within MAF >= ",(1-maf.thr)*100,"%" ))
+  print(table(maf.filter.quick))
+  usemat <- usemat[!maf.filter.quick,]
+  print("Genotype matrix filtered and transformed.")
+  
+  ### start mapping by making decomposition
+  ID <- rownames(letkin) ; length(ID)
+  cbind(ID,use.trait)
+  mod <- lme4qtl::relmatLmer(use.trait ~ (1|ID), relmat = list(ID = letkin))
+  ##Calculate heritability
+  herit.mod <- lme4qtl::VarProp(mod)
+  V <- lme4qtl::varcov(mod, idvar = "ID")
+  V_thr <- V
+  V_thr[abs(V) < 1e-10] <- 0
+  decomp <- wlm::decompose_varcov(V, method = "evd", output = "all")
+  W <- decomp$transform
+  print("Decomposition of covariance matrix was performed.")
+  
+  ## make data object for mapping without any extra factors
+  nnn <- rep(1,ncol(letkin))
+  # class(use.trait)
+  # class(nnn)
+  # class(usemat)
+  ### GWAS with kinship
+  gassoc_gls <- matlm(as.numeric(use.trait) ~ nnn, nnn, pred =  t(usemat), ids = rownames(W), transform = W, batch_size = 1000, verbose = 2,cores = 1,stats_full = T)
+  gassoc_gls$trait.name <- phenotype
+  gassoc_gls$trait.noobs <- length(use.trait)
+  mrkno <- which.max(-log10(gassoc_gls$tab$pval))
+  
+  ###Save as integers
+  gassoc_gls$tab$pval <- as.integer(round(-log10(gassoc_gls$tab$pval)*10000,0))
+  gassoc_gls$tab$zscore <- as.integer(round(gassoc_gls$tab$zscore*10000,0))
+  gassoc_gls$tab$se <- as.integer(round(gassoc_gls$tab$se*10000,0))
+  gassoc_gls$tab$b <- as.integer(round(gassoc_gls$tab$b *10000,0))
+  gassoc_gls <- as.data.frame(gassoc_gls$tab)
+  save(gassoc_gls,file=paste(out.dir,"/GWAS_result_",phenotype,".out",sep=""))
+  save(herit.mod,file=paste(out.dir,"/Heritability_estimate_",phenotype,".out",sep=""))
+  cofac <- usemat[mrkno,]
+  save(cofac,file=paste(out.dir,"/GWAS_cofac.out",sep=""))
+  print("Results saved.")
+  if( give.pval.output.in.R ){
+    return(gassoc_gls)
+  }
+}
+
+########################START SCRIPT#########################
+ext.dir <- "./"
+main.dir <- paste(ext.dir,"/GWAS_Results/",sep="")
+dir.create(main.dir)
+
+###INPUT
+
+#### Load phenotype data
+pheno <- load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phenotypes_for_GWAS.out")
+pheno <- eval(parse(text=pheno))
+rm(phenotypes.diff.mean)
+base.dir <- paste(main.dir, "BGI_",sep="") ##Indicate which variants were used
+
+#Load genotype object for GWAS mapping
+usemat <- load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/obj_bgi.sat.snps.hck.out")
+usemat <- eval(parse(text=usemat))
+snp.info <- usemat[,1:3]
+usemat <- usemat[,2:134]
+rm(sat.snps)
+
+
+#Load kinship matrix
+load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/BGI_Sat_kinship.out")
+
+###Input phenotype
+trait <- rownames(pheno)[3] 
+new.dir <- paste(base.dir,trait,sep="")
+dir.create(new.dir)
+print(colnames(pheno))
+pheno <- pheno[,colnames(pheno) %in% colnames(letkin)]
+print(ncol(pheno))
+
+## Prep sets for GWAS
+log.file.w <- file(paste(new.dir,"/","BGI_",trait,"_warning.log",sep=""),open="wt")
+sink(file=log.file.w,type="message")
+new_letkin <- matrix(as.numeric(letkin),ncol = ncol(letkin),byrow = T)
+letkin_in <- letkin[names(pheno[2,]),names(pheno[2,])] #In case we do not have information for all lines with this phenotype
+usemat_in <- usemat.ori[,names(pheno[2,])] #In case we do not have information for all lines with this phenotype
+
+GWAS(genotypes = usemat_in, trait = as.vector(pheno[trait,]), phenotype.name = trait, kinship=letkin_in, out.dir=new.dir,
+     maf.thr = 0.95,give.pval.output.in.R = F)
+# or 
+GWAS.output <- GWAS(genotypes = usemat_in, trait = as.vector(pheno[trait,]), phenotype.name = trait, kinship=letkin_in, out.dir=new.dir,
+                    maf.thr = 0.95,give.pval.output.in.R = T)
+
+sink(type="message")
+close(log.file.w)
+
+print(paste("GWAS finished. Phenotype is ",trait,sep=""))
+#print(paste(nrow(pheno) - i," traits of ",nrow(pheno)," to go.",sep=""))
+lifecycle::last_lifecycle_warnings()
+
+plot(-log10(GWAS.output$pval/1e4))
 
 #Q3: Plot the manhattan plots for the GWAS run 
 
