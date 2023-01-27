a9589c8fc7f796d25c123baf2658b9cbc140efd1
Mehrem <s.l.mehrem@uu.nl>
2023-01-27 23:07:51 +0100

    Updating draft

--- HackathonScriptDraft.R
+++ HackathonScriptDraft.R
@@ -1,4 +1,6 @@
- ###Draft Hackathon
+###Draft Hackathon
+
+###Draft Hackathon
 
 #### Part 1: Working with phenotypes
 ###########LIBRARIES
@@ -7,18 +9,20 @@ library(ggplot2)
 ##########
 
 ###LIST OF FILES NEEDED FOR THIS PART
-
-#Insert file names
+#Hackathon_Phenotypes_Rep1.xlsx
+#Hackathon_Phenotypes_Rep2.xlsx
+#Species_info.xlsx
 ############
 
-####Loading in your raw phenotype data
-raw.phenotypes.rep1 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Hackathon_Phenotypes_Rep1.xlsx")
-raw.phenotypes.rep2 <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Hackathon_Phenotypes_Rep2.xlsx")
+
+####Loading in your raw phenotype data from excel files
+raw.phenotypes.rep1 <- read.xlsx("./Hackathon_Phenotypes_Rep1.xlsx")
+raw.phenotypes.rep2 <- read.xlsx("./Hackathon_Phenotypes_Rep2.xlsx")
 
 ##Combining replicates
-raw.phenotypes <- rbind(raw.phenotypes.rep1,raw.phenotypes.rep2)
+raw.phenotypes <- rbind(raw.phenotypes.rep1,raw.phenotypes.rep2) #rbind is a function that combines two tables by row
 
-#raw.phenotypes.X <- read.xlsx("PathToExcelSheet") ###Example script for if people want to upload their own phenotypes.
+#raw.phenotypes.X <- read.xlsx("PathToExcelSheet") ###Example if you want to load you own phenotypes
 
 
 ####Exploring the phenotypes
@@ -28,46 +32,73 @@ colnames(raw.phenotypes.rep1) #Different phenotype names
 nrow(raw.phenotypes.rep1) #Number of LK accessions
 
 #Q2: What are the distributions of our phenotypes (by horticultural type)?
-lk.ID <- read.xlsx("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/Species_info.xlsx") ###We load in information about the LK accessions
+lk.ID <- read.xlsx("./Species_info.xlsx") ###We load in information about the LK accessions
 lk.subtype <- lk.ID$Subgroup_SB[match(raw.phenotypes.rep1$accession,lk.ID$LKID)] ###We extract the subtype information by LK ID
 table(lk.subtype) #We look at what subtypes are represented in our set
-##CODE PLOT ## Here Basten if you have code for that I am happy if you want to insert it here
 
-#Q3: What are the mean and median values per LK accession/Horticultural type/replicate?
-##TODO
+## lets check the phenotypic distribution.
+phenotype <-raw.phenotypes$green.trimmed_mean_10.250621 
+horti_types <- lk.ID$Subgroup_SB[match(raw.phenotypes$accession,lk.ID$LKID)] 
+to.plot <- data.frame(horti_types,phenotype)
+ggplot(to.plot,aes(phenotype))+
+  geom_histogram(aes(fill=horti_types),bins = 100)+
+  theme_light()
+
+ggplot(to.plot,aes(phenotype))+
+  geom_histogram(aes(fill=horti_types),bins = 100)+
+  facet_grid(horti_types~.)+
+  theme_light()
+
+
+## lets check the phenotypic distribution over the horticultural types.
+phenotype <-raw.phenotypes$green.trimmed_mean_10.250621 
+horti_types <- lk.ID$Subgroup_SB[match(raw.phenotypes$accession,lk.ID$LKID)] 
+to.plot <- data.frame(horti_types,phenotype)
+ggplot(to.plot,aes(horti_types,phenotype))+
+  geom_boxplot(aes(fill=horti_types))+
+  theme_light()
+
+
+## lets check the phenotypic distribution over the genotypes.
+phenotype <-raw.phenotypes$green.trimmed_mean_10.250621 
+genotypes <- lk.ID$LKID[match(raw.phenotypes$accession,lk.ID$LKID)] 
+to.plot <- data.frame(genotypes,horti_types,phenotype)
+mp.per.g <- aggregate(to.plot$phenotype,list(to.plot$genotypes),mean,na.rm=T)
+to.plot$genotypes <- factor(to.plot$genotypes,levels = mp.per.g$Group.1[order(mp.per.g$x)])
+ggplot(to.plot,aes(genotypes,phenotype))+
+  geom_boxplot(aes(fill=horti_types))+
+  facet_grid(.~horti_types,scale="free_x",space="free_x")+
+  theme_light()
+
 
 
 #####Calculating broad sense heritability (BSH)
-## To get an estimate of how much environment and genetics contribute to trait variation we can calculate the
-## broad sense heritability of our trait of interest. If we know the BSH, we can interpret GWAS results better.
+## To get an estimate of how much environment and genetics contribute to trait variation we can estimate the
+## broad sense heritability of our trait of interest. If we know the approximate BSH, we can interpret GWAS results better.
 ## To calculate BSH, we need the raw phenotype values, including all replicates.
 
-###@Basten, I hope this is the correct way....
-
-pheno.res <- lm(raw.phenotypes$relblue.mean~raw.phenotypes$accession)
+pheno.res <- lm(raw.phenotypes$green.trimmed_mean_10.250621~raw.phenotypes$accession)
 pheno.res.anova <- anova(pheno.res)
-BSH <- pheno.res.anova$`Mean Sq`[1]/(pheno.res.anova$`Mean Sq`[1]+pheno.res.anova$`Mean Sq`[2]) ##Make this a function and then apply?
+
+BSH <- (pheno.res.anova$`Mean Sq`[1]-pheno.res.anova$`Mean Sq`[2])/(pheno.res.anova$`Mean Sq`[1]+pheno.res.anova$`Mean Sq`[2])
 BSH
 
 
-## Our phenotypes are not ready for GWAS yet. We need to normalise the trait (if needed) and prepare it
+## Our phenotypes are not ready for GWAS yet. We need to normalize the trait (if needed) and prepare it
 ## to be used as an input for our GWAS script. For now we will create a phenotype matrix where rows are the phenotypes
 ## and columns are the LK accessions
 
 
-#Q5: What type of measurement do we have? It can be counts, ratios, binary/ordinal values or 
+#Q3: What type of measurement do we have? It can be counts, ratios, binary/ordinal values or 
 ## quantitative measurements.We have replicates so we also have to first take the average across the replicates
-###INSERT CODE and EXPLANATION FOR NORMALISATION
 
-##Here i have some questions: for height, red/blue values etc we do log2? 
-phenotypes.diff.mean <- (raw.phenotypes.rep1[,2:5]+raw.phenotypes.rep2[,2:5])/2
+phenotypes.diff.mean <- (raw.phenotypes.rep1[,-1]+raw.phenotypes.rep2[,-1])/2
 rownames(phenotypes.diff.mean) <- raw.phenotypes.rep1$accession
-phenotypes.diff.mean <- t(phenotypes.diff.mean)
-#Q6: Save phenotype file as input file for GWAS script.
+phenotypes.diff.mean <- t(phenotypes.diff.mean) #We move the table around so each row is a phenotype
 
-save(phenotypes.diff.mean,file="/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/phenotypes_for_GWAS.out")
+#Q4: Save phenotype file as input file for GWAS script.
 
-###Insert CODE for saving
+save(phenotypes.diff.mean,file="./phenotypes_for_GWAS.out")
 
 ############NEW SCRIPT ####################Running GWAS on our phenotypes
 ##Libraries
@@ -75,6 +106,9 @@ save(phenotypes.diff.mean,file="/Users/6186130/Documents/LettuceKnow/Hackathon_2
 library(Matrix)
 library(MASS)
 library(ggplot2)
+library(qqman)
+library(ggplot2)
+library(cowplot)
 # for Step 1: linear mixed model (no SNPs)
 library(lme4qtl) 
 #install.packages("devtools")
@@ -105,14 +139,13 @@ library(dplyr)
 ##for false positive associations by taking into account population structure. We can get an estimate of kinship
 ##calculating the covariance of the SNPs.
 
-##NOT POSSIBLE ON A LAPTOP so they will load the kinship
 
 load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/BGI_Sat_kinship.out")
 heatmap(letkin)
 
-## We prepared our phenotype input and saved it as [FILENAME]
+## We prepared our phenotype input and saved it as phenotypes_for_GWAS.out
 
-## Below you will find the GWAS script we will use.
+## Below you will find the GWAS script we will use. First you run the function as a whole (Line X to Y). Then you can proceed.
 
 GWAS <- function(genotypes, trait, phenotype.name, kinship, out.dir,
                  give.pval.output.in.R = F, maf.thr = 0.95, snp.info) {
@@ -132,7 +165,6 @@ GWAS <- function(genotypes, trait, phenotype.name, kinship, out.dir,
   usemat <- usemat[,selc]
   letkin <- letkin[selc,selc]
   print(dim(letkin))
-  
 
   ## Filter again MAF 5%
   threshold <- round(ncol(usemat)*maf.thr, digits=0)
@@ -239,7 +271,7 @@ rm(sat.snps)
 load("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/BGI_Sat_kinship.out")
 
 ###Input phenotype
-trait <- rownames(pheno)[2] 
+trait <- rownames(pheno)[4] ###TODO:Here I could make it in a for loop...or let them choose by hand
 new.dir <- paste(base.dir,trait,sep="")
 dir.create(new.dir)
 print(colnames(pheno))
@@ -250,8 +282,8 @@ print(ncol(pheno))
 log.file.w <- file(paste(new.dir,"/","BGI_",trait,"_warning.log",sep=""),open="wt")
 sink(file=log.file.w,type="message")
 
-letkin <- letkin[names(pheno[2,]),names(pheno[2,])] #In case we do not have information for all lines with this phenotype
-usemat<- usemat[,names(pheno[2,])] #In case we do not have information for all lines with this phenotype
+letkin <- letkin[names(pheno[4,]),names(pheno[4,])] #In case we do not have information for all lines with this phenotype
+usemat<- usemat[,names(pheno[4,])] #In case we do not have information for all lines with this phenotype
 
 #GWAS(genotypes = usemat_in, trait = as.vector(pheno[trait,]), phenotype.name = trait, kinship=letkin_in, out.dir=new.dir,
      #maf.thr = 0.95,give.pval.output.in.R = F)
@@ -272,14 +304,13 @@ lifecycle::last_lifecycle_warnings()
 ##
 ##INSERT MANHATTAN PLOT CODE
 
-library(qqman)
-library(dplyr)
-load("/Users/6186130/Documents/LettuceKnow/LKHackathon2023/GWAS_Results/BGI_relgreen.mean/GWAS_result_relgreen.mean.out")
+
+load("/Users/6186130/Documents/LettuceKnow/LKHackathon2023/GWAS_Results/BGI_green.trimmed_mean_10.250621/GWAS_result_green.trimmed_mean_10.250621.out")
 bf <- -log10(0.05/nrow(gassoc_gls)) #Bonferroni threshold
 gassoc_gls$POS <- gassoc_gls$POS/1000000
-gassoc_gls <- gassoc_gls[-log10(gassoc_gls$pval) >1,]
+gassoc_gls.topl <- gassoc_gls[-log10(gassoc_gls$pval) >1,]
 
-manhattan(gassoc_gls,snp="SNP",chr="CHR",bp = "POS",p = "pval",logp = T,suggestiveline = F,
+manhattan(gassoc_gls.topl,snp="SNP",chr="CHR",bp = "POS",p = "pval",logp = T,suggestiveline = F,
           genomewideline = bf,annotatePval = bf,col = c("royalblue4","skyblue"))
 
 ##############NEW SCRIPT############### GWAS Follow up
@@ -287,18 +318,70 @@ manhattan(gassoc_gls,snp="SNP",chr="CHR",bp = "POS",p = "pval",logp = T,suggesti
 ##Now that we have a locus (or more) that is associated with our phenotype of interest, we can go further
 ##by investigating what genes we find within these loci.
 
-
-
 ##Load gene annotation file
 gene.anno <- read.delim("/Users/6186130/Documents/LettuceKnow/Hackathon_2023/Data/20221208_Lactuca_sativa.annotation_overview.tsv")
 
-gassoc_gls_sig <- gassoc_gls[-log10(gassoc_gls$pval) > bf,]
-peak <- do.call(data.frame,aggregate(POS ~ CHR, gassoc_gls_sig, function(x){ c(min(x),max(x))}))
+gassoc_gls_sig <- gassoc_gls[-log10(gassoc_gls$pval) > bf,] ##Only choosing significant SNPs
+peak <- do.call(data.frame,aggregate(POS ~ CHR, gassoc_gls_sig, function(x){ c(min(x),max(x))})) #We extract the peaks per chromosome
 
-test <- apply(peak,1,function (x) {
+genes.per.peak <- apply(peak,1,function (x) {
   chromosome <- as.character(x[1])
-  start.pos <- x[2]-0.1
-  end.pos <- x[3]+0.1
+  start.pos <- x[2]-0.05
+  end.pos <- x[3]+0.05
   locus.info <- gene.anno[gene.anno$chromosome.number == chromosome & 
               between(gene.anno$start.sequence/1e6,start.pos,end.pos),]})
-test <- do.call(rbind,test)
+genes.per.peak <- do.call(rbind,genes.per.peak)
+
+
+###To zoom into the region and determine specific blocks, we choose the Top SNP of that region, and calculate its 
+###Correlation to all other SNPs in that window. SNPs correlating with each other usually indicates a form of linkage.
+
+locus <- 5 ##Choosing the peak on Chromosome 5
+gassoc_gls_cor <- gassoc_gls_sig[gassoc_gls_sig$CHR ==as.character(locus),] ##Creating the object for the plot
+top.snp <- gassoc_gls_cor[which.max(gassoc_gls_cor$pval),1:3]
+top.snp.mat <- usemat[which(snp.info$CHR == as.character(top.snp$CHR)&snp.info$POS/1e6 == top.snp$POS),]
+
+##Here we use a function to get all SNPs within the window of interest (with 50kb up and downstream included)
+snp.mat.to.cor <- as.numeric(apply(peak[peak$CHR==locus,],1,function (x) { 
+  chromosome <- as.character(x[1])                                       
+  start.pos <- x[2]-0.05
+  end.pos <- x[3]+0.05
+  snp.to.cor <- which(snp.info$CHR == chromosome &
+                           between(snp.info$POS/1e6,start.pos,end.pos))
+  return(snp.to.cor)
+}
+  ))
+
+usemat.cor <- usemat[snp.mat.to.cor,] ##Selecting the SNPs from the genotype matrix
+snp.info.cor <- snp.info[snp.mat.to.cor,] ###And their positional info
+
+top.snp.cor <- cor(top.snp.mat,t(usemat.cor)) #Correlating the Top SNP to all others
+snp.cor.to.pl <- cbind(as.numeric(top.snp.cor),snp.info.cor)
+snp.cor.to.pl$POS <- snp.cor.to.pl$POS/1e6
+snp.cor.to.pl <- merge(snp.cor.to.pl, gassoc_gls, by=c("CHR","POS"), all.x=TRUE)
+colnames(snp.cor.to.pl)[3]<- "correlation.topsnp"
+
+mhpl <- ggplot(snp.cor.to.pl[between(snp.cor.to.pl$POS,92.5,93.2),],aes(POS,-log10(pval)))+
+  geom_point(aes(colour = correlation.topsnp),alpha=1)+
+  scale_colour_gradient2(low = "chocolate", high="darkgreen",mid="skyblue1")+
+  xlab("Position (Mbp)") + ylab("-log10(p)") +
+  theme_cowplot()+
+  geom_hline(yintercept=7.52, linetype='dotted', col = 'red',size=1)+
+  
+  theme(panel.border = element_rect(colour = "black",linetype = "solid"), 
+              axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+            text=element_text(size=15))
+  
+mhpl
+anno.plot <- ggplot(test[which(test$chromosome.number == as.character(locus))&between(test$start.sequence/1e6,92.5,93.2),])+
+ geom_segment(aes(x=start.sequence/1e6, xend=stop.sequence/1e6, y=chromosome.number, yend=chromosome.number,color=type), 
+              size=5,alpha=0.8)+
+ xlab("Position (in Mb)") +
+ ylab("Annotation")+
+  theme(text=element_text(size=15))
+  
+
+full.locus.plot <- plot_grid(mhpl,anno.plot,ncol = 1,align = "v",rel_heights = c(2,0.5))
+full.locus.plot
